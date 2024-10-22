f47f731d0b0e55b7e7c7bdf2beaa3ece895fc183
Augustin <augustinvathonne@gmail.com>
2024-10-22 13:25:22 +0200

    style(header): burger menu

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const burgerMenu = document.getElementById('burger-menu');
+    const dropdownMenu= document.getElementById('dropdown-menu');
+    burgerMenu.classList.toggle('active');
+    dropdownMenu.classList.toggle('active');
+}
