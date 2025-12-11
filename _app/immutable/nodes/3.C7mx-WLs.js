8a57e100f05a1a7db4f1a10b539690cb1df515b1
Augustin <augustinvathonne@gmail.com>
2025-12-11 18:17:20 +0100

    Updates

---
+++ _app/immutable/nodes/3.C7mx-WLs.js
@@ -0,0 +1,25 @@
+import{s as d,n as r}from"../chunks/BhQRmSu1.js";import{S as v,i as h,d as i,b,a as c,k as l,l as g,c as u,g as f,m as y,h as p,j as _}from"../chunks/DmMi2jSe.js";function P(m){let e,n,t,o=`<img class="svg-wave svelte-1mcvr1p" src="/images/waves/before-presentation-wave.svg" alt="Présentation"/> <div class="about-me-container svelte-1mcvr1p"><div class="about-me svelte-1mcvr1p"><div class="about-me-container svelte-1mcvr1p"><img class="portrait portrait-about-me svelte-1mcvr1p" src="/images/cecile-vathonne-portrait.png" alt="portrait de Cécile Vathonne"/> <div class="about-me-text svelte-1mcvr1p"><h1 class="svelte-1mcvr1p">Mon parcours</h1> <p class="svelte-1mcvr1p">Diplômée de l&#39;Université Paris V Descartes, j&#39;ai suivi une formation de Psychologie clinique,
+						Psychopathologie et Psychanalyse (PCPP). Je dispose donc du titre de <b>psychologue
+						clinicienne</b> et
+						je suis référencée auprès de l&#39;Agence Régionale de santé (numéro RPPS : 10009020818).
+						<br/> <br/>
+						Grâce aux heures de stage clinique effectuées lors de ce cursus, je suis également habilitée à
+						occuper la position de <b>psychothérapeute</b>.
+						<br/> <br/>
+						En complémentarité de cette activité, j&#39;occupe un poste institutionnel.
+						Je travaille auprès des équipes d&#39;écoles primaires et je reçois des enfants avec leurs parents
+						pour différentes problématiques ou symptômes (difficultés d&#39;apprentissage, trouble de
+						l&#39;attention, énurésie, changements familiaux, etc.)
+						<br/> <br/>
+						J&#39;exerce également des missions d&#39;enseignement à l&#39;université Paris Cité.
+						<br/><br/>
+						J&#39;ai décidé de poursuivre ma formation au sein d&#39;une école de psychanalyse pour enrichir mes
+						connaissances cliniques et théoriques. J&#39;évolue donc depuis quelques années au sein du Réseau
+						pour la
+						Psychanalyse à l&#39;Hôpital <b>(RPH - École de psychanalyse)</b>. Je participe aux colloques, aux
+						supervisions et aux groupes de
+						lecture proposés.
+						<br/><br/>
+						Je suis également une psychanalyse personnelle, ce qui permet, entre autres,
+						d&#39;assurer mon discernement auprès des personnes qui viennent me rencontrer en consultation.</p></div></div></div></div> <div class="ending-container svelte-1mcvr1p"><img class="svg-wave svelte-1mcvr1p" src="/images/waves/before-take-appointment-wave.svg" alt="Prendre rendez-vous"/> <div class="doctolib-container"><a href="https://www.doctolib.fr/psychologue/paris/cecile-vathonne/booking?bookingFunnelSource=external_referral&amp;utm_campaign=website-button&amp;utm_source=cecile-vathonne-website-button&amp;utm_medium=referral&amp;utm_content=withoutpreview-blue-floating-bottom-right&amp;utm_term=cecile-vathonne" class="doctolib-button-main" target="_blank" rel="noopener">Prendre rendez-vous
+				<img style="vertical-align:middle;width:auto;height:19px;margin-left:8px" src="https://pro.doctolib.fr/external_button/doctolib-white-transparent.png" alt="Doctolib"/></a></div></div>`;return{c(){e=p("meta"),n=_(),t=p("main"),t.innerHTML=o,this.h()},l(s){const a=g("svelte-186573z",document.head);e=u(a,"META",{name:!0,content:!0}),a.forEach(i),n=f(s),t=u(s,"MAIN",{"data-svelte-h":!0}),y(t)!=="svelte-gt98do"&&(t.innerHTML=o),this.h()},h(){document.title="À propos - Cécile Vathonne",l(e,"name","description"),l(e,"content","Parcours de Cécile Vathonne, psychothérapeute clinicienne et psychanalyste à Paris 9e.")},m(s,a){b(document.head,e),c(s,n,a),c(s,t,a)},p:r,i:r,o:r,d(s){s&&(i(n),i(t)),i(e)}}}class q extends v{constructor(e){super(),h(this,e,null,P,d,{})}}export{q as component};
