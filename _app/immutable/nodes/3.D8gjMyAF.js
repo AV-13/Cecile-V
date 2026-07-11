f639440b4bdce393218f4c5db00dcf57349500f3
Augustin Vathonne <augustin.vathonne@icebergdatalab.com>
2026-07-11 20:51:12 +0200

    Updates

---
+++ _app/immutable/nodes/3.D8gjMyAF.js
@@ -0,0 +1,24 @@
+import{s as S,n as A,r as w,a as $}from"../chunks/CiaGevmY.js";import{S as N,i as k,d as o,k as z,l as B,m as G,b as t,a as T,n as O,o as c,p as U,c as u,g as P,e as q,q as R,r as F,h as p,j as x,u as K}from"../chunks/CoXeqroa.js";import{D as Q}from"../chunks/DlT-Vyrj.js";import{r as H}from"../chunks/CI_HdfQI.js";function W(L){let a,v,n,i,r,J='<div class="portrait-wrapper svelte-3h8h1p"><div class="breathing-halo"></div> <div class="arch-frame portrait svelte-3h8h1p"><img src="/images/cecile-vathonne-portrait.png" alt="portrait de Cécile Vathonne"/></div></div>',C,l,M=`<h1 class="svelte-3h8h1p">Mon parcours</h1> <p class="svelte-3h8h1p">Diplômée de l&#39;Université Paris V Descartes, j&#39;ai suivi une formation de Psychologie clinique,
+				Psychopathologie et Psychanalyse (PCPP). Je dispose donc du titre de <b>psychologue
+				clinicienne</b> et
+				je suis référencée auprès de l&#39;Agence Régionale de santé (numéro RPPS : 10009020818).
+				<br/> <br/>
+				Grâce aux heures de stage clinique effectuées lors de ce cursus, je suis également habilitée à
+				occuper la position de <b>psychothérapeute</b>.
+				<br/> <br/>
+				En complémentarité de cette activité, j&#39;occupe un poste institutionnel.
+				Je travaille auprès des équipes d&#39;écoles primaires et je reçois des enfants avec leurs parents
+				pour différentes problématiques ou symptômes (difficultés d&#39;apprentissage, trouble de
+				l&#39;attention, énurésie, changements familiaux, etc.)
+				<br/> <br/>
+				J&#39;exerce également des missions d&#39;enseignement à l&#39;université Paris Cité.
+				<br/><br/>
+				J&#39;ai décidé de poursuivre ma formation au sein d&#39;une école de psychanalyse pour enrichir mes
+				connaissances cliniques et théoriques. J&#39;évolue donc depuis quelques années au sein du Réseau
+				pour la
+				Psychanalyse à l&#39;Hôpital <b>(RPH - École de psychanalyse)</b>. Je participe aux colloques, aux
+				supervisions et aux groupes de
+				lecture proposés.
+				<br/><br/>
+				Je suis également une psychanalyse personnelle, ce qui permet, entre autres,
+				d&#39;assurer mon discernement auprès des personnes qui viennent me rencontrer en consultation.</p>`,D,s,f,E,h,d,b,V,j;return d=new Q({}),{c(){a=p("meta"),v=x(),n=p("main"),i=p("section"),r=p("div"),r.innerHTML=J,C=x(),l=p("div"),l.innerHTML=M,D=x(),s=p("div"),f=p("hr"),E=x(),h=p("div"),K(d.$$.fragment),this.h()},l(e){const m=U("svelte-186573z",document.head);a=u(m,"META",{name:!0,content:!0}),m.forEach(o),v=P(e),n=u(e,"MAIN",{});var g=q(n);i=u(g,"SECTION",{class:!0});var _=q(i);r=u(_,"DIV",{class:!0,"data-svelte-h":!0}),R(r)!=="svelte-iuu2la"&&(r.innerHTML=J),C=P(_),l=u(_,"DIV",{class:!0,"data-svelte-h":!0}),R(l)!=="svelte-18754qi"&&(l.innerHTML=M),_.forEach(o),D=P(g),s=u(g,"DIV",{class:!0});var y=q(s);f=u(y,"HR",{class:!0}),E=P(y),h=u(y,"DIV",{class:!0});var I=q(h);F(d.$$.fragment,I),I.forEach(o),y.forEach(o),g.forEach(o),this.h()},h(){document.title="À propos - Cécile Vathonne",c(a,"name","description"),c(a,"content","Parcours de Cécile Vathonne, psychothérapeute clinicienne et psychanalyste à Paris 9e."),c(r,"class","about-visual svelte-3h8h1p"),c(l,"class","about-me-text svelte-3h8h1p"),c(i,"class","about-me section-shell svelte-3h8h1p"),c(f,"class","hairline"),c(h,"class","doctolib-container svelte-3h8h1p"),c(s,"class","ending-container svelte-3h8h1p")},m(e,m){t(document.head,a),T(e,v,m),T(e,n,m),t(n,i),t(i,r),t(i,C),t(i,l),t(n,D),t(n,s),t(s,f),t(s,E),t(s,h),O(d,h,null),b=!0,V||(j=[$(H.call(null,r)),$(H.call(null,l,{delay:.15})),$(H.call(null,s))],V=!0)},p:A,i(e){b||(G(d.$$.fragment,e),b=!0)},o(e){B(d.$$.fragment,e),b=!1},d(e){e&&(o(v),o(n)),o(a),z(d),V=!1,w(j)}}}class te extends N{constructor(a){super(),k(this,a,null,W,S,{})}}export{te as component};
---
+++ _app/immutable/nodes/6.CyGZm0zy.js
@@ -0,0 +1,14 @@
+import{s as h,n as o}from"../chunks/CiaGevmY.js";import{S as m,i as v,d as n,b as q,a as u,o as a,p as b,c as r,g,q as f,h as p,j as _}from"../chunks/CoXeqroa.js";function P(d){let e,i,s,c=`<div class="container svelte-uql6oi"><h1 class="svelte-uql6oi">Mentions légales</h1> <section class="svelte-uql6oi"><h2 class="svelte-uql6oi">Éditeur du site</h2> <p class="svelte-uql6oi"><strong>Cécile Vathonne</strong><br/>
+				Psychothérapeute - Psychologue clinicienne<br/>
+				33 Rue Jean-Baptiste Pigalle<br/>
+				75009 Paris<br/>
+				Téléphone : 07 62 80 25 77<br/>
+				Email : cecile.vathonne@gmail.com<br/>
+				RPPS : 10009020818</p></section> <section class="svelte-uql6oi"><h2 class="svelte-uql6oi">Hébergement</h2> <p class="svelte-uql6oi">Ce site est hébergé sur GitHub Pages.<br/>
+				Adresse : github.com</p></section> <section class="svelte-uql6oi"><h2 class="svelte-uql6oi">Responsabilité</h2> <p class="svelte-uql6oi">L&#39;éditeur du site s&#39;efforce de garantir l&#39;exactitude et la mise à jour des informations
+				publiées, mais ne peut en aucun cas être tenu responsable des erreurs, omissions ou
+				utilisation inappropriée du contenu.</p></section> <section class="svelte-uql6oi"><h2 class="svelte-uql6oi">Propriété intellectuelle</h2> <p class="svelte-uql6oi">Tous les contenus du site (textes, images, vidéos) sont la propriété exclusive de Cécile
+				Vathonne ou utilisés avec autorisation. Toute reproduction, même partielle, sans
+				consentement préalable est interdite.</p></section> <section class="svelte-uql6oi"><h2 class="svelte-uql6oi">Données personnelles</h2> <p class="svelte-uql6oi">Pour toute question concernant le traitement de vos données personnelles, consultez notre
+				<a href="/data-protection" class="svelte-uql6oi">politique de confidentialité</a>.</p></section> <section class="svelte-uql6oi"><h2 class="svelte-uql6oi">Liens externes</h2> <p class="svelte-uql6oi">Le site peut contenir des liens vers d&#39;autres sites. Cécile Vathonne n&#39;est pas
+				responsable du contenu de ces sites externes.</p></section></div>`;return{c(){e=p("meta"),i=_(),s=p("main"),s.innerHTML=c,this.h()},l(t){const l=b("svelte-1iugcaw",document.head);e=r(l,"META",{name:!0,content:!0}),l.forEach(n),i=g(t),s=r(t,"MAIN",{class:!0,"data-svelte-h":!0}),f(s)!=="svelte-mcjb21"&&(s.innerHTML=c),this.h()},h(){document.title="Mentions légales - Cécile Vathonne",a(e,"name","description"),a(e,"content","Mentions légales du site de Cécile Vathonne, psychothérapeute."),a(s,"class","svelte-uql6oi")},m(t,l){q(document.head,e),u(t,i,l),u(t,s,l)},p:o,i:o,o,d(t){t&&(n(i),n(s)),n(e)}}}class M extends m{constructor(e){super(),v(this,e,null,P,h,{})}}export{M as component};
